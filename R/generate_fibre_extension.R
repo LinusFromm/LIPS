239e787d5a5c8c427f530085ea6690dc7b13d2bc
LinusFromm <linus.fromm18@gmail.com>
2026-02-03 08:03:12 +1300

    Add generate_fibre_extension.R and .Rd

---
+++ R/generate_fibre_extension.R
@@ -0,0 +1,56 @@
+#' Compute a specific p-fibre extension
+#' @param A Configuration matrix
+#' @param y Observation vector
+#' @param p Relaxation limit
+#' @param epsilon Tolerance for off-integerness
+#'
+#' @return Returns a Matrix containing all points of the fibre as columns.
+#' @export
+generate_fibre_extension <- function(A,
+                                     y,
+                                     p,
+                                     epsilon = 1e-04){
+  x_max = c()
+  x_min = c()
+
+  for(i in 1:ncol(A)){
+    x_max[i] = lpSolve::lp(direction = "max",
+                           objective.in = diag(ncol(A))[i,],
+                           const.mat = A,
+                           const.dir = "=",
+                           const.rhs = y,
+                           all.int = TRUE)$solution[i]
+    x_min[i] = lpSolve::lp(direction = "min",
+                           objective.in = diag(ncol(A))[i,],
+                           const.mat = A,
+                           const.dir = "=",
+                           const.rhs = y,
+                           all.int = TRUE)$solution[i]
+  }
+
+  idx = c(1)
+  i = 2
+  while(qr(A[,idx])$rank < qr(A)$rank){
+    temp = A[,c(idx,i)]
+    if(qr(temp)$rank > qr(A[,idx])$rank){
+      idx = c(idx, i)
+    }
+    i = i+1
+  }
+
+  hyperrectangle_ext_coord = setdiff(1:ncol(A), idx)
+  coords = apply(rbind(x_max, x_min, p)[,hyperrectangle_ext_coord], 2, function(col) return((col[2]-col[3]):(col[1]+length(hyperrectangle_ext_coord)*col[3])), simplify = FALSE)
+
+  grid = expand.grid(coords)
+  A1 = A[,idx]
+  A2 = A[,hyperrectangle_ext_coord]
+
+  ext = c(solve(A1)%*%y)-solve(A1)%*%A2%*%t(grid)
+  fibre_idx = which(apply(ext, 2, function(col) all(round(col) >= -p[hyperrectangle_ext_coord]) && all(abs(col - round(col)) <= epsilon)))
+
+  fibre = matrix(0, ncol = length(fibre_idx), nrow = ncol(A))
+  fibre[idx,] = round(ext[,fibre_idx])
+  fibre[hyperrectangle_ext_coord,] = round(t(grid)[,fibre_idx])
+
+  return(fibre)
+}
